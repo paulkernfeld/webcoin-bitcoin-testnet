6f1cfa87591113e159db19d110a791541f6128ca
Paul Kernfeld <paulkernfeld@gmail.com>
2016-07-05 17:33:40 -0400

    Add localhost as a static peer
    This makes it easier to do offline dev w/ real data

--- src/net.js
+++ src/net.js
@@ -1,6 +1,7 @@
 var magic = 0x0709110b
 var defaultPort = 18333
 
+var staticPeers = ['localhost']
 var dnsSeeds = [
   'testnet-seed.bitcoin.petertodd.org',
   'testnet-seed.bluematt.me',
@@ -17,6 +18,7 @@ var webSeeds = [
 module.exports = {
   magic,
   defaultPort,
+  staticPeers,
   dnsSeeds,
   webSeeds
 }
